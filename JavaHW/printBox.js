8a518d5fdf0575a7ddd55a65e0e7f8cde4300bc9
AlexFisher <efishentcoding@gmail.com>
2020-02-04 21:45:26 -0600

    javascriptHW1

--- JavaHW/printBox.js
+++ JavaHW/printBox.js
@@ -1,17 +1,12 @@
 // assignment 1
-
-
 // function madlib(name,subject) {
 //     return name + '\'s favorite subject in school is ' + subject + '.';
 // }
 // var result = madlib('Alex', 'Math');
-
 // console.log(result)
-
 // assignment 2
-
 // function tipAmount(bill,service){
-//     if (service == 'good') { 
+//     if (service == 'good') {
 //         return 'The tip is $' + bill * .20          
 //     } else if (service == 'fair') {
 //         return 'The tip is $' + bill * .15          
@@ -21,11 +16,8 @@
 //         return bill * 0;                       
 //     }
 // }
-
 // console.log(tipAmount(10,'good'));
-
 // assignment3
-
 // function totalAmount(bill,service){
 //    if (service == 'good') {
 //        tipAmount = bill * .20;                
@@ -46,19 +38,14 @@
 //    }
 // }
 // console.log( totalAmount(100,'good'));
-
 // assignment 4
-
 // function printNumbers(start,end) {
 //     for (let n = start; n <= end; n++) {
 //         console.log(n);
 //     }
 // }
-
 // printNumbers(1,10);
-
 // assignment 5
-
 // drawSquare(5);
 // function drawSquare(five){
 //   var square="";
@@ -70,67 +57,23 @@
 //     square="";
 //   } 
 // }
-
-// // assignment 6
-// function printBox(width, height) {
-//     for (let i = 0; i < height; i++) {    
-//         if (i === 0 || i === height -1) {  
-//             console.log('*'.repeat(width));  
-//         } else {        
-//             console.log('*' + ' '.repeat(width-2) + '*');
-//     }
-// }
-// }
-// printBox(6, 4);
-
-
-// // assignment 7
-// function printBanner(Text) {
-// console.log('*'.repeat(Text.length+4))  
-// console.log('* ' + Text + ' *');
-// console.log('*'.repeat(Text.length+4)) 
-// }
-// printBanner('All this coding is making my head hurt')
-
-//assignment 8
-function leetSpeak(leettext) {
-    let newText = "";
-    for (let i = 0; i < leettext.length; i++) {
-      let character = leettext[i];
-      switch (character.toUpperCase()) {
-        case "A": {
-          character = "4";
-          break;
+// assignment 6
+function printBox(width, height) {
+    for (let i = 0; i < height; i++) {
+        if (i === 0 || i === height - 1) {
+            console.log('*'.repeat(width));
         }
-        case "E": {
-          character = "3";
-          break;
+        else {
+            console.log('*' + ' '.repeat(width - 2) + '*');
         }
-        case "G": {
-          character = "6";
-          break;
-        }
-        case "L": {
-          character = "1";
-          break;
-        }
-        case "O": {
-          character = "0";
-          break;
-        }
-        case "S": {
-          character = "5";
-          break;
-        }
-        case "T": {
-          character = "7";
-          break;
-        }
-      }
-      newText += character;
     }
-  
-    return newText;
-  }
-  
-  console.log(leetSpeak("Leet"));
+    console.log(printBox(6, 4));
+
+    // assignment 7
+    // function printBanner(Text) {
+    // console.log('*'.repeat(Text.length+4))  
+    // console.log('* ' + Text + ' *');
+    // console.log('*'.repeat(Text.length+4)) 
+    // }
+    // printBanner('All this coding is making my head hurt');
+}
